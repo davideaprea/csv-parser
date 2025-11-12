6375b58c9e5040b265c0f8a58b61440282887cbb
daviaprea <apredav@gmail.com>
2025-11-12 18:01:05 +0100

    in normal field state

--- src/main/java/csvparser/state/InNormal.java
+++ src/main/java/csvparser/state/InNormal.java
@@ -1,6 +1,7 @@
 package csvparser.state;
 
 import csvparser.enumeration.CSVColumnSeparator;
+import csvparser.exception.UnexpectedCharacterException;
 
 public class InNormal extends ParsingState {
     protected InNormal(CSVColumnSeparator separator, StringBuilder stringBuilder) {
@@ -9,31 +10,38 @@ public class InNormal extends ParsingState {
 
     @Override
     protected ParsingState evalNormalCharacter(char character) {
-        return null;
+        stringBuilder.append(character);
+
+        return this;
     }
 
     @Override
     protected ParsingState evalWhiteSpace(char character) {
-        return null;
+        stringBuilder.append(character);
+
+        return this;
     }
 
     @Override
     protected ParsingState evalLineFeed() {
-        return null;
+        throw new UnexpectedCharacterException('\n', "LF characters should only appear in quoted fields or after a CR character.");
     }
 
     @Override
     protected ParsingState evalCarriageReturn() {
-        return null;
+        return new CarriageReturn(separator, stringBuilder);
     }
 
     @Override
     protected ParsingState evalSeparator() {
-        return null;
+        return new ColumnEnd(separator, stringBuilder);
     }
 
     @Override
     protected ParsingState evalQuotes() {
-        return null;
+        throw new UnexpectedCharacterException(
+                '"',
+                "Double quotes can only be be used to wrap a column field or escaping another double quotes character"
+        );
     }
 }
