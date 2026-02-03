9ed5dad76468ed4f08655c8bb6e6cc196aefdf51
daviaprea <apredav@gmail.com>
2026-02-03 11:58:36 +0100

    CSVRowParser class def

---
+++ src/main/java/csvparser/parser/CSVRowParser.java
@@ -0,0 +1,47 @@
+package csvparser.parser;
+
+import csvparser.builder.RowBuilder;
+import csvparser.enumeration.CSVColumnSeparator;
+import csvparser.state.ParsingContext;
+import csvparser.state.ParsingState;
+import csvparser.state.RowEnd;
+import csvparser.state.RowInit;
+import csvparser.structure.CSVRow;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Optional;
+
+public class CSVRowParser {
+    private final Reader input;
+    private final CSVColumnSeparator separator;
+
+    public CSVRowParser(Reader input, CSVColumnSeparator separator) {
+        this.input = input;
+        this.separator = separator;
+    }
+
+    private Optional<CSVRow> next() throws IOException {
+        ParsingContext parsingContext = new ParsingContext(
+                new RowBuilder(),
+                separator
+        );
+        ParsingState parsingState = new RowInit(parsingContext);
+        int currentCharacter = -1;
+
+        while (
+                !(parsingState instanceof RowEnd) &&
+                (currentCharacter = input.read()) >= 0
+        ) {
+            parsingState = parsingState.eval((char) currentCharacter);
+        }
+
+        final CSVRow newRow = parsingState.end();
+
+        if (currentCharacter == -1 && newRow.columnsNumber() == 0) {
+            return Optional.empty();
+        }
+
+        return Optional.of(newRow);
+    }
+}
