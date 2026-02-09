d02f66efb880ed3d329c394a26c73f8e363325c4
daviaprea <apredav@gmail.com>
2026-02-09 10:22:38 +0100

    display name for tests

--- src/test/java/integration/loader/TestCaseLoader.java
+++ src/test/java/integration/loader/TestCaseLoader.java
@@ -9,6 +9,8 @@ import integration.loader.source.ValidRowsTestCaseSource;
 import integration.parser.testcase.ExceptionTestCase;
 import integration.parser.testcase.ValidHeadedRowTestCase;
 import integration.parser.testcase.ValidRowTestCase;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.params.provider.Arguments;
 import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
@@ -22,43 +24,57 @@ import java.util.stream.StreamSupport;
 public class TestCaseLoader {
     private final static String BASE_PATH = "testcases/%s";
 
-    public static Stream<ValidRowTestCase> validRows() {
+    public static Stream<Arguments> validRows() {
         return fromPath(
                 ValidRowsTestCaseSource.class,
                 BASE_PATH.formatted("valid/valid_rows_test_cases.yaml")
-        ).map(testCase -> new ValidRowTestCase(
-                new StringReader(testCase.getInput()),
-                testCase.getOutput().stream().map(Row::new).toList()
-        ));
+        ).map(source -> {
+                    var testCase = new ValidRowTestCase(
+                            new StringReader(source.getInput()),
+                            source.getOutput().stream().map(Row::new).toList()
+                    );
+
+                    return Arguments.of(Named.of(source.getName(), testCase));
+                }
+        );
     }
 
-    public static Stream<ValidHeadedRowTestCase> validHeadedRows() {
+    public static Stream<Arguments> validHeadedRows() {
         return fromPath(
                 ValidHeadedRowsTestCaseSource.class,
                 BASE_PATH.formatted("valid/valid_headed_rows_test_cases.yaml")
-        ).map(testCase -> new ValidHeadedRowTestCase(
-                new StringReader(testCase.getInput()),
-                testCase.getOutput().getRows()
-                        .stream()
-                        .map(row -> new HeadedRow(
-                                new Row(row),
-                                testCase.getOutput().getHeaders()
-                        ))
-                        .toList()
-        ));
+        ).map(source -> {
+                    var testCase = new ValidHeadedRowTestCase(
+                            new StringReader(source.getInput()),
+                            source.getOutput().getRows()
+                                    .stream()
+                                    .map(row -> new HeadedRow(
+                                            new Row(row),
+                                            source.getOutput().getHeaders()
+                                    ))
+                                    .toList()
+                    );
+
+                    return Arguments.of(Named.of(source.getName(), testCase));
+                }
+        );
     }
 
-    public static Stream<ExceptionTestCase> unexpectedCharacterTestCase() {
+    public static Stream<Arguments> unexpectedCharacterTestCase() {
         return fromPath(
                 UnexpectedCharacterTestCaseSource.class,
                 BASE_PATH.formatted("invalid/unexpected_character_test_cases.yaml")
-        ).map(testCase -> new ExceptionTestCase(
-                new StringReader(testCase.getInput()),
-                new UnexpectedCharacterException(
-                        testCase.getException().getCharacter().charAt(0),
-                        testCase.getException().getMessage()
-                )
-        ));
+        ).map(source -> {
+                    var testCase = new ExceptionTestCase(
+                            new StringReader(source.getInput()),
+                            new UnexpectedCharacterException(
+                                    source.getException().getCharacter().charAt(0),
+                                    source.getException().getMessage()
+                            ));
+
+                    return Arguments.of(Named.of(source.getName(), testCase));
+                }
+        );
     }
 
     private static <T> Stream<T> fromPath(Class<T> testCaseClass, String path) {
