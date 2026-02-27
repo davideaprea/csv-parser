075823ddf5f5380952fff7e9f72e97271b5d0058
daviaprea <apredav@gmail.com>
2026-02-27 14:49:48 +0100

    column separator docs

--- src/main/java/csv/enumeration/ColumnSeparator.java
+++ src/main/java/csv/enumeration/ColumnSeparator.java
@@ -1,11 +1,17 @@
 package csv.enumeration;
 
+/**
+ * Enumeration representing the supported column separator characters.
+ */
 public enum ColumnSeparator {
     COMMA(','),
     SEMICOLON(';'),
     TAB('\t'),
     PIPE('|');
 
+    /**
+     * The character symbol representing the column separator.
+     */
     public final char symbol;
 
     ColumnSeparator(final char symbol) {
